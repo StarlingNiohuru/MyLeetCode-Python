3c0077623a64abc204a23da66ee67e3f3303d37d
StarlingNiohuru <sider.niohuru@gmail.com>
2021-12-08 20:04:50 +0000

    p0088 finished

---
+++ p0088_MergeSortedArray.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i, j, k = m - 1, n - 1, len(nums1) - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] >= nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        if j >= 0:
+            nums1[0:k + 1] = nums2[0:j + 1]
