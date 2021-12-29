556285e4550466be6c715d215903bb22f760f5a4
StarlingNiohuru <sider.niohuru@gmail.com>
2021-12-29 01:48:49 +0000

    p0160 finished

---
+++ p0160_IntersectionofTwoLinkedLists.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        p1, p2 = headA, headB
+        while p1 != p2:
+            p1 = p1.next if p1 else headB  # "if p1" can make p1 stopping at NULL, avoiding infinite loop
+            p2 = p2.next if p2 else headA
+        return p1
