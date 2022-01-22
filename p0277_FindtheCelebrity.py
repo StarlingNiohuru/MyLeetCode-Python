a4b29001441438b8eb3427e6c42887b6f31e2fc3
StarlingNiohuru <sider.niohuru@gmail.com>
2022-01-22 00:15:34 +0000

    p0277 finished

--- p0277_FindtheCelebrity.py
+++ p0277_FindtheCelebrity.py
@@ -15,3 +15,22 @@ class Solution:
             else:
                 return i
         return -1
+
+
+# In the graph matrix, celebrity has all 1 col and all 0 row.
+# From (0,n-1) search the candidate on the diagonal and verify it.
+class Solution2:
+
+    def findCelebrity(self, n: int) -> int:
+        i, j = 0, n - 1
+        while i < j:
+            if knows(i, j):
+                i += 1
+            else:
+                j -= 1
+        for k in range(n):
+            if k == i:
+                continue
+            if knows(i, k) or not knows(k, i):
+                return -1
+        return i
